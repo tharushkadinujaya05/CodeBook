7220070d7da5101424af554b3a1ad28c10112b40
Tharushka Dinujaya <dtdkumara@students.nsbm.ac.lk>
2024-08-28 01:55:43 +0000

    chore: Add VariableSwap program to Java basics

---
+++ Java/javabasics/src/VariableSwap.java
@@ -0,0 +1,12 @@
+public class VariableSwap {
+    public static void main (String[] args){
+        int a = 10;
+        int b = 20;
+        int temp;
+        System.out.println("Before swapping a = " + a + " b = " + b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping a = " + a + " b = " + b);
+    }
+}
