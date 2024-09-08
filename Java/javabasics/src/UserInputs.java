a3e2dfdce811a7decaa69c9bc4c884b18f3eb832
Tharushka Dinujaya <dtdkumara@students.nsbm.ac.lk>
2024-09-08 17:35:37 +0000

    UserInputs program in Java

---
+++ Java/javabasics/src/UserInputs.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; // Import the Scanner class
+public class UserInputs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object
+        System.out.println("What is ur name?"); 
+        String name = scanner.nextLine(); // Read user input
+        System.out.println("How old are you?");
+        int age = scanner.nextInt(); // Read user input
+
+        System.out.println("Hello "+ name+" You are"+age+" Years old"); // Output user input
+    }
+}
